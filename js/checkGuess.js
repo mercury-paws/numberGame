fb22ead181391d1cba471068830fb1e94ff2fee4
mercury-paws <145927427+mercury-paws@users.noreply.github.com>
2024-11-08 10:00:53 +0100

    polishing

--- js/checkGuess.js
+++ js/checkGuess.js
@@ -1,14 +1,9 @@
-// takes two parameters - the player's guess and the correct number.
-// It should return a string indicating whether the guess is too low,
-// too high, or correct.
-
-
 export function checkGuess(playerNumber, generatedNumber) {
-    if (playerNumber < generatedNumber) {
-        return "Too Low";
-    } else if (playerNumber > generatedNumber) {
-        return "Too High";
-    } else {
-        return "Correct";
-    }
+  if (playerNumber < generatedNumber) {
+    return "Too Low";
+  } else if (playerNumber > generatedNumber) {
+    return "Too High";
+  } else {
+    return "Correct";
+  }
 }
--- js/generateRandomNumber.js
+++ js/generateRandomNumber.js
@@ -1,8 +1,3 @@
-// function returns a random integer between 1 and 100.
-// This will be the number the player has to guess.
-
-// export function generateRandomNumber() {}
-
 export function generateRandomNumber() {
-    return Math.floor(Math.random() * 100) + 1;
+  return Math.floor(Math.random() * 100) + 1;
 }
